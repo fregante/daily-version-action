fc42011d3ec684c59b63230a75a77f4be9143825
Federico Brigante <opensource@bfred.it>
2020-05-31 21:59:28 +0200

    Cleanup and testing

--- distribution/index.js
+++ distribution/index.js
@@ -56,22 +56,41 @@ const util = __webpack_require__(669);
 const execFile = util.promisify(__webpack_require__(129).execFile);
 
 async function init() {
-	await execFile('git', ['fetch', '--tags']);
-	const {stdout: tagOnHead} = await execFile('git', ['tag', '-l', '--points-at', 'HEAD']);
-	if (tagOnHead) {
-		console.log('::set-output name=version::' + tagOnHead);
-		console.log('No new commits since the last tag');
+	throw new Error('Testing an error');
+
+	// Use ENV if it's a `push.tag` event
+	if (process.env.GITHUB_REF.startsWith('refs/tags/')) {
+		const pushedTag = process.env.GITHUB_REF.replace('refs/tags/', '');
+		console.log('::set-output name=version::' + process.env.GITHUB_REF.replace('refs/tags/', ''));
+		console.log('Run triggered by tag `' + pushedTag + '`. No new tags will be created by `daily-version-action`.');
 		return;
 	}
 
-	const {stdout: version} = await execFile('npx', ['daily-version']);
+	// Look for tags on the current commit
+	await execFile('git', ['--depth=1', 'origin', 'refs/tags/*:refs/tags/*']);
+	const {stdout: tagsOnHead} = await execFile('git', ['tag', '-l', '--points-at', 'HEAD']);
+	if (tagsOnHead) {
+		const [mostRecentTag] = tagsOnHead.split('/');
+		console.log('::set-output name=version::' + mostRecentTag);
+		console.log('No new commits since the last tag (' + mostRecentTag + '). No new tags will be created by `daily-version-action`.');
+		return;
+	}
+
+	// A new tag must br created
+	const {stdout} = await execFile('npx', ['daily-version']);
+	const version = stdout.trim(); // `stdout` ends with \n
 	console.log('::set-output name=version::' + version);
+	console.log(JSON.stringify(version));
 	await execFile('git', ['tag', version, '-m', version]);
-	await execFile('git', ['push', 'origin', '-m', version]);
+	await execFile('git', ['push', 'origin', version]);
 	console.log('::set-output name=created::yes');
 }
 
-init();
+init().catch(error => {
+	console.error('why is this not being caught?');
+	console.error(error);
+	process.exit(1); // eslint-disable-line unicorn/no-process-exit
+});
 
 
 /***/ }),
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const util = require('util');
 const execFile = util.promisify(require('child_process').execFile);
 
 async function init() {
+	throw new Error('Testing an error');
+
 	// Use ENV if it's a `push.tag` event
 	if (process.env.GITHUB_REF.startsWith('refs/tags/')) {
 		const pushedTag = process.env.GITHUB_REF.replace('refs/tags/', '');
@@ -14,17 +16,24 @@ async function init() {
 	await execFile('git', ['--depth=1', 'origin', 'refs/tags/*:refs/tags/*']);
 	const {stdout: tagsOnHead} = await execFile('git', ['tag', '-l', '--points-at', 'HEAD']);
 	if (tagsOnHead) {
-		const [mostRecentTag] = tagOnHead.split('/');
+		const [mostRecentTag] = tagsOnHead.split('/');
 		console.log('::set-output name=version::' + mostRecentTag);
 		console.log('No new commits since the last tag (' + mostRecentTag + '). No new tags will be created by `daily-version-action`.');
 		return;
 	}
 
-	const {stdout: version} = await execFile('npx', ['daily-version']);
+	// A new tag must br created
+	const {stdout} = await execFile('npx', ['daily-version']);
+	const version = stdout.trim(); // `stdout` ends with \n
 	console.log('::set-output name=version::' + version);
+	console.log(JSON.stringify(version));
 	await execFile('git', ['tag', version, '-m', version]);
-	await execFile('git', ['push', 'origin', '-m', version]);
+	await execFile('git', ['push', 'origin', version]);
 	console.log('::set-output name=created::yes');
 }
 
-init();
+init().catch(error => {
+	console.error('why is this not being caught?');
+	console.error(error);
+	process.exit(1); // eslint-disable-line unicorn/no-process-exit
+});
