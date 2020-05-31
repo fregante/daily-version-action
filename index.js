f2d9b7491afcdd29613e03de6f36b0c7b3de434e
Fregante <opensource@bfred.it>
2020-05-31 20:41:06 +0200

    Various fixes
    According to #3

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const util = require('util');
 const execFile = util.promisify(require('child_process').execFile);
 
 async function init() {
-	await execFile('git', ['fetch', '--tags']);
-	const {stdout: tagOnHead} = await execFile('git', ['tag', '-l', '--points-at', 'HEAD']);
-	if (tagOnHead) {
-		console.log('::set-output name=version::' + tagOnHead);
-		console.log('No new commits since the last tag');
+	// Use ENV if it's a `push.tag` event
+	if (process.env.GITHUB_REF.startsWith('refs/tags/')) {
+		const pushedTag = process.env.GITHUB_REF.replace('refs/tags/', '');
+		console.log('::set-output name=version::' + process.env.GITHUB_REF.replace('refs/tags/', ''));
+		console.log('Run triggered by tag `' + pushedTag + '`. No new tags will be created by `daily-version-action`.');
+		return;
+	}
+
+	// Look for tags on the current commit
+	await execFile('git', ['--depth=1', 'origin', 'refs/tags/*:refs/tags/*']);
+	const {stdout: tagsOnHead} = await execFile('git', ['tag', '-l', '--points-at', 'HEAD']);
+	if (tagsOnHead) {
+		const [mostRecentTag] = tagOnHead.split('/');
+		console.log('::set-output name=version::' + mostRecentTag);
+		console.log('No new commits since the last tag (' + mostRecentTag + '). No new tags will be created by `daily-version-action`.');
 		return;
 	}
 
